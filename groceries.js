31ba2800a81abc6dfe29925c45e3e144a79c28c2
Annaliskirwa <annaliskirwa@gmail.com>
2021-09-14 13:22:06 +0300

    It will add a function for resetting the form

--- groceries.js
+++ groceries.js
@@ -5,3 +5,8 @@ function Groceries(first, second, third){
     this.thirdtem = third;
     this.items = [];
 }
+function resetFields(){
+    $("input#new-first-item").val("");
+    $("input#new-second-item").val("");
+    $("input#new-third-item").val("");
+}

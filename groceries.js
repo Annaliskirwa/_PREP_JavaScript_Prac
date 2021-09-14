2256987d84dfbc5b83750b014f242501af058ab8
Annaliskirwa <annaliskirwa@gmail.com>
2021-09-14 15:48:58 +0300

    It will add an input field for extra groceries

--- groceries.js
+++ groceries.js
@@ -1,34 +1,13 @@
-//business logic
-function Groceries(first, second, third){
-    this.firstItem = first;
-    this.secondItem = second;
-    this.thirdtem = third;
-    this.items = [];
-}
-function resetFields(){
-    $("input#new-first-item").val("");
-    $("input#new-second-item").val("");
-    $("input#new-third-item").val("");
-}
 //user interface logic
 $(document).ready(function(){
     $("#add-groceries").click(function(){
-        $("#new-groceriesitems").append(
-            '<div class="new-address">' +
-                                 '<div class="form-group">' +
-                                   '<label for="forth-item">Forth item</label>' +
-                                   '<input type="text" class="form-control forth-item">' +
-                                 '</div>' +
-                                 '<div class="form-group">' +
-                                   '<label for="fifth-item">Fifth item</label>' +
-                                   '<input type="text" class="form-control fifth-item">' +
-                                 '</div>' +
-                                 '<div class="form-group">' +
-                                   '<label for="sixth-item">Sixth item</label>' +
-                                   '<input type="text" class="form-control sixth-item">' +
-                                 '</div>' +
-                               '</div>'
-        );
-    });
-    
-})
+        var html = '';
+        html += '<div class="form-group">';
+        html += '<label for = "first-item">Another item</label>';
+        html += ' <input type = "text" class = "form-control" id = "new-first-item">';
+        html += '</div>';
+        html += '</div>';
+
+        $('#new-groceries').append(html);
+    })
+});

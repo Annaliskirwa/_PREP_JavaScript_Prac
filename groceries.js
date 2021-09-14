d91cbe05fd22e04cea963970e9c0a717a0239de8
Annaliskirwa <annaliskirwa@gmail.com>
2021-09-14 13:30:20 +0300

    It will add form fields for other items

--- groceries.js
+++ groceries.js
@@ -10,3 +10,25 @@ function resetFields(){
     $("input#new-second-item").val("");
     $("input#new-third-item").val("");
 }
+//user interface logic
+$(document).ready(function(){
+    $("#add-groceries").click(function(){
+        $("#new-groceriesitems").append(
+            '<div class="new-address">' +
+                                 '<div class="form-group">' +
+                                   '<label for="forth-item">Forth item</label>' +
+                                   '<input type="text" class="form-control forth-item">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="fifth-item">Fifth item</label>' +
+                                   '<input type="text" class="form-control fifth-item">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="sixth-item">Sixth item</label>' +
+                                   '<input type="text" class="form-control sixth-item">' +
+                                 '</div>' +
+                               '</div>'
+        );
+    });
+    
+})

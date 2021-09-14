af2e020a350ca76e9c86b55b16ff5182474e26d9
Annaliskirwa <annaliskirwa@gmail.com>
2021-09-14 17:21:33 +0300

    It will display the groceries in a list

--- groceries.js
+++ groceries.js
@@ -3,11 +3,23 @@ $(document).ready(function(){
     $("#add-groceries").click(function(){
         var html = '';
         html += '<div class="form-group">';
-        html += '<label for = "first-item">Another item</label>';
-        html += ' <input type = "text" class = "form-control" id = "new-first-item">';
+        html += '<label for = "another-item">Another item</label>';
+        html += ' <input type = "text" class = "form-control" id = "new-another-item">';
         html += '</div>';
         html += '</div>';
 
         $('#new-groceries').append(html);
     })
+    $("button").click(function(event){
+        var item1 = $("#new-first-item").val();
+        var item2 = $("#new-second-item").val();
+        var item3 = $("#new-third-item").val();
+        var itemanother = $("#new-another-item").val();
+        $("#groceries1").text(item1)
+        $("#groceries2").text(item2)
+        $("#groceries3").text(item3)
+        $("#groceries4").text(itemanother)
+        $("#show-groceries").show();
+        event.preventDefault();
+    });
 });

4ad3342d7830ee8b1de8bcca81246a489333aedc
Annaliskirwa <annaliskirwa@gmail.com>
2021-09-14 11:52:53 +0300

    It will display the puzzle

--- wordpuzzle.js
+++ wordpuzzle.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
-    $("form#wordpuzzle").submit(event){
-        var stringinput = $("input#textinput").val();
-        var inputletters = stringinput.split("");
+    $("form#wordpuzzle").submit(function(event) {
+        var stringinput=$("input#textinput").val();
+        var inputletters=stringinput.split("");
+        console.log(inputletters);
         for(var index=0; index < inputletters.length; index+=1){
             if(inputletters[index]==="a" || inputletters[index]==="e" || inputletters[index]==="i" || inputletters[index]==="o" || inputletters[index]==="u") {
               inputletters[index]="-"
             }
         }
-        var puzzleString = inputletters.join("");
-        $(".outputstring").text(puzzleString);
+        var puzzlestring = inputletters.join("");
+        console.log(puzzlestring)
+        $(".outputstring").text(puzzlestring)
         $("#wordinput").toggle();
         $("#wordoutput").toggle();
         event.preventDefault();
-    }
-})
+      });
+});

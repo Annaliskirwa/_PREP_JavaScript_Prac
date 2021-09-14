12a46f7c86e310f83b9d415736547d7eb8812676
Annaliskirwa <annaliskirwa@gmail.com>
2021-09-15 00:06:55 +0300

    It will  split the input sentense

--- wordplay.js
+++ wordplay.js
@@ -1,4 +1,9 @@
 //user interface logic
 $(document).ready(function(){
-
+    $("form#wordplay").submit(function(event){
+        var sentenseInput = $("textarea#sentenseinput").val();
+        var splitSentense = sentenseInput.split("");
+        console.log(splitSentense);
+        event.preventDefault();
+    });
 });

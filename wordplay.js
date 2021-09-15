454abdc1db09e2feb3baefa529e1bea61dd08d00
Annaliskirwa <annaliskirwa@gmail.com>
2021-09-15 13:53:51 +0300

    It will add a function that will reverse words and not the sentense

--- wordplay.js
+++ wordplay.js
@@ -6,7 +6,25 @@ $(document).ready(function(){
         console.log(splitSentense);
         var reverseSentense = splitSentense.reverse().join("")
         console.log(reverseSentense);
-        $(".outputstring").text(reverseSentense)
+        $(".outputstring1").text(reverseSentense)
         event.preventDefault();
     });
+    $("form#wordplay").submit(function(event){
+        function reverseWords(string){
+            var string = $("textarea#sentenseinput").val();
+            var reversedStrArr = [];
+            stringArr = string.split(' ');
+            
+            stringArr.forEach( word => {
+              var charWordArr = word.split('');
+              var reversedWord = '';
+              for(i = charWordArr.length-1; i >= 0 ; i--){
+                reversedWord += charWordArr[i];
+              }
+              reversedStrArr.push(reversedWord);
+            });
+            return reversedStrArr.join(' ');
+          }
+          console.log(reverseWords())
+    })
 });

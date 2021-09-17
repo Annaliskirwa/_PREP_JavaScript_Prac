fde4cb406d6a827e6e33b3a9c86b138f3097abdc
Annaliskirwa <annaliskirwa@gmail.com>
2021-09-17 11:45:39 +0300

    It will add the withdraw amount prototype

--- bank account/bank.js
+++ bank account/bank.js
@@ -5,8 +5,16 @@ function Account(name, balance) {
   }
 Account.prototype.deposit = function(amount){
     if (this._isPositive(amount)){
-        this.balance =+ amount;
+        this.balance += amount;
         console.log(`Deposit: ${this.name} new balance is ${this.balance}`);
+        return true
+    }
+    return false
+}
+Account.prototype.withdraw = function(amount){
+    if (this._isAllowed(amount)){
+        this.balance -= amount;
+        console.log(`Withdraw: ${this.name} new balance is ${this.balance}`);
         return true
     }
     return false

736527208c21f67cd6a2bf29060fc7eb8e601591
Annaliskirwa <annaliskirwa@gmail.com>
2021-09-17 22:22:16 +0300

    It will add the deposit function prototype

--- bank account/bank2.js
+++ bank account/bank2.js
@@ -3,6 +3,11 @@ BankAccount = function(name, balance){
     this.name = name;
     this.balance = balance;
 };
+
+BankAccount.prototype.deposit = function(amount){
+    this.balance += amount;
+}
+
 BankAccount.prototype.toString = function(){
-    return "Name: " + this.name + " Balance: "+ this.balance;
+    return "Name: " + this.name + " Balance: $"+ this.balance;
 };

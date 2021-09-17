00dfba13a1061eea85d30a3e282f3592e7f7faf6
Annaliskirwa <annaliskirwa@gmail.com>
2021-09-17 22:18:18 +0300

    It will add a prototype that converts details to string

--- bank account/bank2.js
+++ bank account/bank2.js
@@ -1,4 +1,8 @@
+//business logic
 BankAccount = function(name, balance){
     this.name = name;
     this.balance = balance;
-}
+};
+BankAccount.prototype.toString = function(){
+    return "Name: " + this.name + " Balance: "+ this.balance;
+};

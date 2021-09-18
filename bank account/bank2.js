0616ecbf21a63fb7965b97642ee82cd72d865f8f
Annaliskirwa <annaliskirwa@gmail.com>
2021-09-18 17:06:52 +0300

    It will display the name and the acocunt balance

--- bank account/bank2.js
+++ bank account/bank2.js
@@ -15,3 +15,15 @@ BankAccount.prototype.withdraw = function(amount){
 BankAccount.prototype.toString = function(){
     return "Name: " + this.name + " Balance: $"+ this.balance;
 };
+//user interface logic
+$(document).ready(function(){
+    $("form#new-account").submit(function(event){
+        event.preventDefault();
+        var name = $("input#account-name").val();
+        var balance1 = $("input#initial-deposit").val();
+        var balance = parseInt(balance1)
+        var newAccount = new BankAccount(name, balance);
+    $("#output-balance").text(newAccount);
+    console.log(newAccount.toString())
+    })
+})

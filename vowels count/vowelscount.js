431c57c20b2d7045160a0baa2120d858de62cdb6
Annaliskirwa <annaliskirwa@gmail.com>
2021-09-21 21:43:21 +0300

    A few event handler changes

--- vowels count/vowelscount.js
+++ vowels count/vowelscount.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $("form#vowels").submit(function(event){
+        event.preventDefault()
         var sentenseinput = $("textarea#sentense-input").val();
         console.log(sentenseinput);
-        event.preventDefault()
     })
 })

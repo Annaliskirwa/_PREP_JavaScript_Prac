6c879b991f56782cfaf65bdb274b6ed2d28cdc50
Annaliskirwa <annaliskirwa@gmail.com>
2021-09-22 08:36:19 +0300

    The function will check for both lower and upper case vowels

--- vowels count/vowelscount.js
+++ vowels count/vowelscount.js
@@ -1,7 +1,20 @@
 $(document).ready(function(){
     $("form#vowels").submit(function(event){
         event.preventDefault()
-        var sentenseinput = $("textarea#sentense-input").val();
-        console.log(sentenseinput);
+        function getVowels(str){
+        var str = $("textarea#sentense-input").val();
+        console.log(str);
+        var vowelsCount = 0;
+        var string = str.toString();
+        for (var i = 0; i <= string.length - 1; i++) {
+
+            //if a vowel, add to vowel count
+              if (string.charAt(i) == "a" || string.charAt(i) == "e" || string.charAt(i) == "i" || string.charAt(i) == "o" || string.charAt(i) == "u" || string.charAt(i) == "A" || string.charAt(i) == "E" || string.charAt(i) == "I" || string.charAt(i) == "O" || string.charAt(i) == "U") {
+                vowelsCount += 1;
+              }
+            }
+            return vowelsCount;
+          }
+        console.log(getVowels())
     })
 })

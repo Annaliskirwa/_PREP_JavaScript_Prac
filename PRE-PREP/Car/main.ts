a5eb9788b7d9f7c00992d2114ee2df452af66fde
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-12 10:10:08 +0300

    main.ts to run the car app

---
+++ PRE-PREP/Car/main.ts
@@ -0,0 +1,9 @@
+import { Car } from './car';
+import { Engine } from './engine';
+import { Wheels } from './wheels';
+
+function main (){
+    let car = new Car();
+    car.startEngine();
+}
+main()

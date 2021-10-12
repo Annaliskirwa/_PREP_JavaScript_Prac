a6e77a6c58745285fdcdd6d16c0bdc3145d67431
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-12 13:33:00 +0300

    The transpiled ts to js

--- PRE-PREP/Car/car.js
+++ PRE-PREP/Car/car.js
@@ -1,12 +1,10 @@
 "use strict";
 exports.__esModule = true;
 exports.Car = void 0;
-var engine_1 = require("./engine");
-var wheels_1 = require("./wheels");
 var Car = /** @class */ (function () {
-    function Car() {
-        this.engine = new engine_1.Engine();
-        this.wheels = new wheels_1.Wheels();
+    function Car(engine, wheels) {
+        this.engine = engine;
+        this.wheels = wheels;
     }
     Car.prototype.startEngine = function () {
         this.engine.start();
--- PRE-PREP/Car/main.js
+++ PRE-PREP/Car/main.js
@@ -1,8 +1,12 @@
 "use strict";
 exports.__esModule = true;
 var car_1 = require("./car");
+var engine_1 = require("./engine");
+var wheels_1 = require("./wheels");
 function main() {
-    var car = new car_1.Car();
+    var engine = new engine_1.Engine();
+    var wheels = new wheels_1.Wheels();
+    var car = new car_1.Car(engine, wheels);
     car.startEngine();
 }
 main();
